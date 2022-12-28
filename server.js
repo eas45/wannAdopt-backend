b969cc8b9c6ceded91b3a44a551044ea037571db
Esteban Antón Sellés <eas45@alu.ua.es>
2022-12-28 22:23:16 +0100

    Añade rutas para el CRUD de Perfil

--- server.js
+++ server.js
@@ -26,10 +26,60 @@ db.sequelize.sync(/* { force: true } */)
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  // const Profile = db.profiles;
+  // const User = db.users;
+
+  // const profile = {
+  //   email: 'a@a.com',
+  //   password: 'pass'
+  // }
+
+  // const user = {
+  //   name: 'prueba',
+  //   profileId: 1
+  // }
+
+  // Profile.create(profile)
+  //   .then(data => {
+  //     // res.send(data);
+  //   })
+  //   .catch(err => {
+  //     res.json({ message: 'Error: ' + err.message });
+  //   })
+
+  // User.create(user)
+  //   .then(data => {
+  //     res.send(data);
+  //   })
+  //   .catch(() => {
+  //     res.json({ message: 'Error: ' + err.message });
+  //   })
+
+  // Profile.findByPk(1, { include: ['user'] })
+  //   .then((profile) => {
+  //     res.send(profile);
+  //   })
+
+  // User.findByPk(1, { include: ['profile'] })
+  //   .then((user) => {
+  //     res.send(user);
+  //   })
+
+  // Profile.destroy({
+  //   where: {
+  //     id: 1
+  //   }
+  // }).then(() => {
+  //   res.send('OK');
+  // });
+
+  // Profile.restore();
+
+  res.json({ message: 'Welcome to bezkoder application.' });
+
 });
 
-// require("./app/routes/animal.routes")(app);
+require("./app/routes/profile.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
