2beb21afcba8bedaeae2c4a52d9e035ca80b87d5
Esteban Antón Sellés <eas45@alu.ua.es>
2023-01-01 19:17:22 +0100

    Quita comentario de debug en registro

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -3,7 +3,6 @@ const Profile = db.profiles;
 const ProfileController = require('../controllers/profile.controller');
 
 exports.register = async (req, res) => {
-  console.log('Registro recibido');
   const profileResponse = await ProfileController._create(req);
 
   return res.status(profileResponse.status).send(profileResponse.payload);
