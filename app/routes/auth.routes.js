bc81f943cc91c731bb7dd48e5f1f5b0c35c0249d
Esteban Antón Sellés <eas45@alu.ua.es>
2023-01-03 00:08:43 +0100

    Añade middlewares a las rutas de auth

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,13 +1,45 @@
-module.exports = app => {
-  const auth = require('../controllers/auth.controller');
+const { verifyRegister } = require('../middleware');
+const auth = require('../controllers/auth.controller');
+var router = require('express').Router();
 
-  var router = require('express').Router();
+module.exports = app => {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, Content-Type, Accept"
+    );
+    next();
+  });
 
   // Register
-  router.post('/register', auth.register);
+  router.post(
+    '/register',
+    [verifyRegister.checkDuplicateEmail],
+    auth.register);
 
   // Login
   router.post('/login', auth.login);
 
+  // Logout
+  router.post('/logout', auth.logout);
+
   app.use('/api/auth', router);
 }
+
+// module.exports = function(app) {
+//   app.use(function(req, res, next) {
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, Content-Type, Accept"
+//     );
+//     next();
+//   });
+
+//   app.post(
+//     '/register',
+//     [
+//       verifyRegister.checkDuplicateEmail
+//     ],
+//     auth.register
+//   );
+// }
