c5565ef6910da21be984bcee70947e6dfb468250
Esteban Antón Sellés <eas45@alu.ua.es>
2023-01-03 00:10:37 +0100

    Añade tiempos de expiración de token y refresh

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  secret: 'wannAdopt-secret-key'
+  secret: 'wannAdopt-secret-key',
+  // jwtExpiration: 3600,
+  // jwtRefreshExpiration: 86400
+  /* for test */
+  jwtExpiration: 60,          // 1 minute
+  jwtRefreshExpiration: 120,  // 2 minutes
 }
