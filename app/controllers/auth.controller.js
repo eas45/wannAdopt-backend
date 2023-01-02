bfaac95786bda2387090eb90ae53ca401a386690
Esteban Antón Sellés <eas45@alu.ua.es>
2023-01-02 23:07:21 +0100

    Añade cifrado de las contraseñas en el registro

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,12 +1,16 @@
 const db = require('../models');
 const Profile = db.profiles;
 const ProfileController = require('../controllers/profile.controller');
+const config = require('../config/auth.config');
+
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 exports.register = async (req, res) => {
   const profileResponse = await ProfileController._create(req);
 
   return res.status(profileResponse.status).send({ email: profileResponse.payload.email });
-}
+};
 
 exports.login = async (req, res) => {
   if (!req.body.email) {
@@ -20,12 +24,35 @@ exports.login = async (req, res) => {
   
   try {
     const profile = await Profile.findOne({
-      where: { email, password }
+      where: { email }
     });
 
-    return profile ?
-      res.send({ message: 'Guay' }) :
-      res.status(404).send({ message: 'Credenciales incorrectas.' });
+    if (!profile) {
+      return res.status(404).send({
+        message: 'Correo electrónico no encontrado.'
+      });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(
+      password, profile.password
+    );
+
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        message: 'Contraseña incorrecta.'
+      })
+    }
+
+    const token = jwt.sign({ id: profile.id }, config.secret, {
+      expiresIn: 86400 // 24 hours
+    });
+
+    req.session.token = token;
+
+    return res.send({
+      id: profile.id,
+      email: profile.email
+    });
   } catch (err) {
     console.log(err.message);
 
@@ -33,4 +60,15 @@ exports.login = async (req, res) => {
       message: 'No se pudo iniciar sesión. Por favor, inténtelo más tarde.'
     });
   }
-}
+};
+
+exports.logout = async (req, res) => {
+  try {
+    req.session = null;
+    return res.send({
+      message: 'Sesión cerrada.'
+    });
+  } catch (err) {
+    this.next(err);
+  }
+};
