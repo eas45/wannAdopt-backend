f4e5cae6098f00d83cc9a7cd1aff1bc95825870a
Esteban Antón Sellés <eas45@alu.ua.es>
2023-01-02 23:03:03 +0100

    Configura el uso de las cookies

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const db = require("./app/models");
+const cookieSession = require("cookie-session");
 
 const app = express();
 
@@ -16,6 +17,14 @@ app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    name: 'profile-session',
+    secret: 'secreto',
+    httpOnly: true
+  })
+)
 
 db.sequelize.sync(/* { force: true } */)
   .then(() => {
