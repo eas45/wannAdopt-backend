37d08eb69936998d69108b1ad99dbd68e8922ba3
Esteban Antón Sellés <eas45@alu.ua.es>
2023-01-08 05:53:57 +0100

    Añade Helmet

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const helmet = require('helmet');
 const db = require("./app/models");
 const cookieSession = require("cookie-session");
 
@@ -19,6 +20,7 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(morgan('tiny'));
+app.use(helmet());
 
 // parse requests of content-type - application/json
 app.use(express.json());
